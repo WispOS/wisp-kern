5d5b01a1faafd2538027cc552a22525d165a62ac
GhostedGaming <epicrobloxgamer41@gmail.com>
2026-02-05 13:02:13 -0500

    commiting what i have

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -4,8 +4,13 @@
 pub mod gdt;
 pub mod hardware;
 pub mod idt;
-pub mod native_drivers;
 pub mod sysio;
+pub mod native_drivers;
 pub mod workspace;
 
 // extern crate alloc;
+pub mod idt;
+
+pub fn init() {
+    idt::init_idt();
+}
